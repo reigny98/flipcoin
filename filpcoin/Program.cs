0692873440703b994be2a078305eb75b489c2002
reigny98 <reigny2016@gmail.com>
2022-03-03 17:52:16 -1200

    added filpcoin program

---
+++ filpcoin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace filpcoin
+{
+    internal class FlipCoin
+    {
+        int headcount = 0;
+        int tailcount = 0;
+        public string FlipCoinMethod(int userinput)
+        {
+            Random r = new Random();
+            while (userinput !=0)
+            {
+                double random=r.NextDouble();
+
+                if (random <= 0.5)
+                {
+                    tailcount = tailcount + 1;
+                }
+                else
+                {
+                    headcount ++;  
+                }
+                userinput = userinput - 1;
+            }
+            return "%";
+        }
+    }
+}
